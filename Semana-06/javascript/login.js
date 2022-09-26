fbc8ae731fb5f7eab55fd22427aa95749b72b0ec
EmaMuises <emanuelmuises95@gmail.com>
2022-09-25 23:16:37 -0300

    BaSP-W06-2022: create login js file, validation email and password, create sing-up form

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -5,36 +5,52 @@ window.onload = function() {
     var inputUserPassword = document.getElementById('input-user-password');
     var paragraphSecondInput = document.getElementById('error-second-paragraph');
 
+    // Email
     paragraphFirstInput.classList.add('hidden');
+
+    function removeBorder(color){
+        if(inputTextEmail.classList.contains(color)){
+            inputTextEmail.classList.remove(color);
+        }
+        
+    }
 
     inputTextEmail.onblur = function () {
         if (inputTextEmail.value.match(emailExpression)) {
+            removeBorder('red-border')
             inputTextEmail.classList.add('green-border');
             paragraphFirstInput.classList.add('hidden');
         } else {
+            removeBorder('green-border')
             inputTextEmail.classList.add('red-border');
             paragraphFirstInput.classList.remove('hidden');
         };
     };
 
     inputTextEmail.onfocus = function () {
-        inputTextEmail.classList.remove('green-border') || inputTextEmail.classList.remove('red-border');
-    };
-
-    inputTextEmail.onfocus = function () {
+        inputTextEmail.classList.remove('green-border'); 
+        inputTextEmail.classList.remove('red-border');
         paragraphFirstInput.classList.add('hidden');
     };
 
+    // Password
+    var passExp = /^[A-Za-z0-9]*$/;
+
     paragraphSecondInput.classList.add('hidden');
 
-    // inputUserPassword.onblur = function () {
-    //     if (inputUserPassword.value < 8) {
-    //         inputUserPassword.classList.add('red-border');
-    //         paragraphSecondInput.classList.remove('hidden');
-    //     } else {
-    //         inputUserPassword.classList.add('green-border');
-    //         paragraphSecondInput.classList.add('hidden');
-    //     }
-    // };
+    function removeBorderTwo(color){
+        if (inputUserPassword.classList.contains(color)) {
+            inputUserPassword.classList.remove(color);
+        };
+    };
 
+    inputUserPassword.onblur = function () {
+        if (inputUserPassword.value.match(passExp)) {
+            inputUserPassword.classList.add('green-border');
+        } else if (!inputUserPassword.value.match(passExp)) {
+            inputUserPassword.classList.add('red-border');
+        } else if (inputUserPassword.value == '') {
+            inputUserPassword.classList.add('red-border');
+        }
+    };
 };
---
+++ Semana-06/javascript/sing-up.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+
+    var firstName = document.getElementById('first-name');
+    console.log(firstName);
+
+};
